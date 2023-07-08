020cf1724c43dc578888d9b58a9129ddc2addbfa
GioyeTheDev <138115736+GioyeTheDev@users.noreply.github.com>
2023-07-08 18:17:25 +0200

    Add files via upload

---
+++ No Access To System.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This Virus only works in PCs that have only 1 user.
+echo or it will be useless.
+echo this virus didn't never ever got tested.
+pause
+echo die at your risk.
+/sg /t 5 /c YOU ARE A IDIOT! HAHAHAHAHAHAHAHA! WHY DID YOU OPEN ME? LET ME SLEEP!
